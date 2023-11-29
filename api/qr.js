6af2566cf5df2cfb885342cf6edb8bc9acf0ac3d
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-11-29 19:07:55 +0700

    Update qr.js

--- api/qr.js
+++ api/qr.js
@@ -3,13 +3,11 @@ const QRCode = require('qrcode');
 const { PassThrough } = require('stream');
 const generatePayload = require('promptpay-qr');
 
-const mobileNumber ='';
 const router = express.Router();
-let bruh = '';
 
 router.get('/', async (req, res) => {
     let amount = req.query.amount;
-    mobileNumber = req.query.number;
+    let mobileNumber = req.query.number;
     amount = Number(amount);
     let payload = generatePayload(mobileNumber, { amount });
     const qrStream = new PassThrough();
